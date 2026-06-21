45b8ba3a0939e81a3a86188f7998cc05d3824aa7
Sam-007-007 <yshubham25@iitk.ac.in>
2026-06-21 11:38:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import tempfile
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 from fingerprint import identify_song
 
@@ -69,3 +71,56 @@ if uploaded_file is not None:
         ax.set_title("Offset Histogram")
 
         st.pyplot(fig)
+
+
+st.header("Batch Mode")
+
+batch_files = st.file_uploader(
+    "Upload multiple audio clips",
+    type=["mp3", "wav"],
+    accept_multiple_files=True
+)
+
+if batch_files:
+
+    results = []
+
+    for uploaded_file in batch_files:
+
+        with tempfile.NamedTemporaryFile(delete=False) as tmp:
+
+            tmp.write(uploaded_file.read())
+            filename = tmp.name
+
+        result = identify_song(filename)
+
+        if result is not None:
+
+            song_name, _, _, _ = result
+
+            # remove extension from prediction
+            prediction = os.path.splitext(song_name)[0]
+
+        else:
+
+            prediction = "No Match"
+
+        results.append(
+            {
+                "filename": uploaded_file.name,
+                "prediction": prediction
+            }
+        )
+
+    df = pd.DataFrame(results)
+
+    st.write(df)
+
+    csv = df.to_csv(index=False)
+
+    st.download_button(
+        label="Download results.csv",
+        data=csv,
+        file_name="results.csv",
+        mime="text/csv"
+    )
